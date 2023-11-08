6bbef598891192410069e07479d0af737fdef37b
Linda Drucker <pw22l006@technikum-wien.at>
2023-11-08 15:35:49 +0100

    fix: fixed encoding on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,4 @@
 <template>
-    <div id="header">
-        <h1>Über uns</h1>
-        </div>
         <br />
         <div id="content">
             <b-container class="card-container">
@@ -9,17 +6,17 @@
                     <b-card img-src="src\assets\restaurant.jpg" img-height="400px" img-alt="Restaurant" img-top border-variant="secondary"
                             header="Unser Restaurant" header-border-variant="secondary" align="center">
                         <b-card-text>
-                            In unserem prämierten Restaurant können Sie den Tag gemütlich bei einem Dinner ausklingen lassen. Die Küche ist regional und saisonal, vegetarische und vegane Optionen sind selbstverständlich verfügbar. Ob á la Carte oder 5-Gänge-Menü mit Weinbegleitung: hier kommt jede und jeder auf seine Kosten.
-                            Die Einrichtung ist exquisit und geschmackvoll, das Personal exzellent ausgebildet. Lassen Sie sich verwöhnen vom eleganten Ambiente, von erlesenen Speisen und von der großen Auswahl an außergewöhnlichen Getränken.
+                            In unserem prÃ¤mierten Restaurant kÃ¶nnen Sie den Tag gemÃ¼tlich bei einem Dinner ausklingen lassen. Die KÃ¼che ist regional und saisonal, vegetarische und vegane Optionen sind selbstverstÃ¤ndlich verfÃ¼gbar. Ob Ã¡ la Carte oder 5-GÃ¤nge-MenÃ¼ mit Weinbegleitung: hier kommt jede und jeder auf seine Kosten.
+                            Die Einrichtung ist exquisit und geschmackvoll, das Personal exzellent ausgebildet. Lassen Sie sich verwÃ¶hnen vom eleganten Ambiente, von erlesenen Speisen und von der groÃŸen Auswahl an auÃŸergewÃ¶hnlichen GetrÃ¤nken.
                         </b-card-text>
                     </b-card>
 
                     <b-card img-src="src\assets\wellness02.jpg" img-alt="Wellnessbereich" img-height="400px" img-top border-variant="secondary"
                             header="Der Wellnessbereich" header-border-variant="secondary" align="center">
                         <b-card-text>
-                            Der Wellnessbereich unseres Hotels ist ein moderner und luxuriöser Rückzugsort, der Ihnen ultimative Entspannung und Erholung bietet. Dieser idyllische Ort wurde mit viel Liebe zum Detail gestaltet, um eine Ruheoase zu schaffen, in der Sie dem Alltagsstress entfliehen können.
+                            Der Wellnessbereich unseres Hotels ist ein moderner und luxuriÃ¶ser RÃ¼ckzugsort, der Ihnen ultimative Entspannung und Erholung bietet. Dieser idyllische Ort wurde mit viel Liebe zum Detail gestaltet, um eine Ruheoase zu schaffen, in der Sie dem Alltagsstress entfliehen kÃ¶nnen.
 
-                            Unsere Einrichtung ist geprägt von zeitgemäßem Design und hochwertigen Materialien, die ein Gefühl von Eleganz und Luxus vermitteln. Große, bodentiefe Fenster ermöglichen einen atemberaubenden Ausblick auf Stadt, während Sie sich in einer entspannten Atmosphäre verwöhnen lassen. Unser geschultes Personal steht Ihnen zur Seite, um sicherzustellen, dass Ihr Aufenthalt zu einem unvergesslichen Erlebnis wird.
+                            Unsere Einrichtung ist geprÃ¤gt von zeitgemÃ¤ÃŸem Design und hochwertigen Materialien, die ein GefÃ¼hl von Eleganz und Luxus vermitteln. GroÃŸe, bodentiefe Fenster ermÃ¶glichen einen atemberaubenden Ausblick auf Stadt, wÃ¤hrend Sie sich in einer entspannten AtmosphÃ¤re verwÃ¶hnen lassen. Unser geschultes Personal steht Ihnen zur Seite, um sicherzustellen, dass Ihr Aufenthalt zu einem unvergesslichen Erlebnis wird.
                         </b-card-text>
                     </b-card>
                 </b-card-group>
@@ -28,8 +25,8 @@
                     <b-card img-src="src\assets\wellness.jpg" img-alt="Unser Wellnessbereich" img-top border-variant="secondary" 
                             header="Der Wellnessbereich" header-border-variant="secondary" align="center">
                         <b-card-text>
-                            Genießen Sie die modernen Annehmlichkeiten, die Ihnen zur Verfügung stehen, darunter Whirlpools, Dampfbäder und ein Fitnesscenter, das mit den neuesten Geräten ausgestattet ist. Das helle und luftige Ambiente unseres Wellnessbereichs trägt dazu bei, dass Sie sich vom ersten Moment an wohlfühlen und den Stress des Alltags hinter sich lassen können.
-                            Wir laden Sie ein, in unsere luxuriöse Wellnessoase einzutauchen, in der Entspannung und Erholung an erster Stelle stehen. Gönnen Sie sich eine Auszeit und erleben Sie die Ruhe und Gelassenheit, die unser Wellnessbereich bietet. Wir freuen uns darauf, Sie bei uns begrüßen zu dürfen.
+                            GenieÃŸen Sie die modernen Annehmlichkeiten, die Ihnen zur VerfÃ¼gung stehen, darunter Whirlpools, DampfbÃ¤der und ein Fitnesscenter, das mit den neuesten GerÃ¤ten ausgestattet ist. Das helle und luftige Ambiente unseres Wellnessbereichs trÃ¤gt dazu bei, dass Sie sich vom ersten Moment an wohlfÃ¼hlen und den Stress des Alltags hinter sich lassen kÃ¶nnen.
+                            Wir laden Sie ein, in unsere luxuriÃ¶se Wellnessoase einzutauchen, in der Entspannung und Erholung an erster Stelle stehen. GÃ¶nnen Sie sich eine Auszeit und erleben Sie die Ruhe und Gelassenheit, die unser Wellnessbereich bietet. Wir freuen uns darauf, Sie bei uns begrÃ¼ÃŸen zu dÃ¼rfen.
                         </b-card-text>
                     </b-card>
                 </b-row>
